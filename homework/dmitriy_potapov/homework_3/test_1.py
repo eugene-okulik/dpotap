b5628cf38cb5472a5c48b2b72f7a532365431154
dpotap90 <125043689+dpotap90@users.noreply.github.com>
2024-08-28 21:01:17 +0300

    homework_3

---
+++ homework/dmitriy_potapov/homework_3/test_1.py
@@ -0,0 +1,6 @@
+a = 5
+b = 10
+
+print(a + b)
+print(b - a)
+print(a * b)
---
+++ homework/dmitriy_potapov/homework_3/test_3.py
@@ -0,0 +1,10 @@
+from math import sqrt
+
+x = 5
+y = 10
+
+arifmetic_mean = (x + y) / 2
+print(arifmetic_mean)
+
+geometric_mean = sqrt(x * y)
+print(geometric_mean)
