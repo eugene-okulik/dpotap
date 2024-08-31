9516e9ed6af833e4f30aefc26a551b7582be2aa6
dpotap90 <125043689+dpotap90@users.noreply.github.com>
2024-08-31 23:39:18 +0300

    homework_5 update flake8 github

--- homework/dmitriy_potapov/homework_5/homework_5.py
+++ homework/dmitriy_potapov/homework_5/homework_5.py
@@ -6,13 +6,13 @@ result_1 = 'результат операции: 42'
 result_2 = 'результат операции: 514'
 result_3 = 'результат работы программы: 9'
 
-num_1 = int(result_1[result_1.index(':')+2:])
+num_1 = int(result_1[result_1.index(':') + 2:])
 print(num_1 + 10)
 
-num_2 = int(result_2[result_2.index(':')+2:])
+num_2 = int(result_2[result_2.index(':') + 2:])
 print(num_2 + 10)
 
-num_3 = int(result_3[result_3.index(':')+2:])
+num_3 = int(result_3[result_3.index(':') + 2:])
 print(num_3 + 10)
 
 students = ['Ivanov', 'Petrov', 'Sidorov']
